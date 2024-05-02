3f78f6a158ccc5960f71f8d397beb9a75ff663a8
rivacortezf <u202215313@upc.edu.pe>
2024-05-02 03:13:20 -0500

    refactor: change package

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
